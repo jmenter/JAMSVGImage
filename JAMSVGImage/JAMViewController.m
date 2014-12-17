6e3f1af731edbec949da11865c3839fd9eb6f8a2
Jeff Menter <jmenter@gmail.com>
2014-12-16 19:25:10 -0600

    made main view controller use ibdesignable/inspectable JAMSVGImageView

--- JAMSVGImage/JAMViewController.m
+++ JAMSVGImage/JAMViewController.m
@@ -1,26 +1,6 @@
 
 #import "JAMViewController.h"
-#import "JAMSVGImageView.h"
 
-@interface JAMViewController ()
-@property (nonatomic) JAMSVGImageView *svgImageView;
-@end
 @implementation JAMViewController
-
-- (void)viewDidLoad;
-{
-    [super viewDidLoad];
-    self.view.backgroundColor = UIColor.whiteColor;
-    JAMSVGImage *image = [JAMSVGImage imageNamed:@"paths-data-03-f"];
-    self.svgImageView = [JAMSVGImageView.alloc initWithSVGImage:image];
-    self.svgImageView.contentMode = UIViewContentModeScaleAspectFit;
-    
-    [self.view addSubview:self.svgImageView];
-}
-
-- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
-{
-    self.svgImageView.frame = CGRectMake(0, 0, [touches.anyObject locationInView:self.view].x, [touches.anyObject locationInView:self.view].y);
-}
 
 @end
