1cd3056fd491665aabeb39f57ee5585d245977a5
Jeff Menter <jmenter@gmail.com>
2014-12-17 23:26:13 -0600

    changed demo svg

--- JAMSVGImage/JAMViewController.m
+++ JAMSVGImage/JAMViewController.m
@@ -3,4 +3,9 @@
 
 @implementation JAMViewController
 
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event;
+{
+    self.svgImageView.frame = CGRectMake(0, 0, [touches.anyObject locationInView:self.view].x, [touches.anyObject locationInView:self.view].y);
+}
+
 @end
