7ed3f79b6f997fe04a93975ce114d009e284a4fe
Jeff Menter <jmenter@gmail.com>
2014-12-19 16:08:53 -0600

    formatting changes

--- Classes/JAMSVGImage/JAMSVGUtilities.m
+++ Classes/JAMSVGImage/JAMSVGUtilities.m
@@ -256,47 +256,34 @@ CGPoint CGPointSubtractPoints(CGPoint point1, CGPoint point2)
 
 - (NSData *)gunzip:(NSError**)error;
 {
-    /*
-     * A minimal gzip header/trailer is 18 bytes long.
-     * See: RFC 1952 http://www.gzip.org/zlib/rfc-gzip.html
-     */
-    if(self.length < 18)
-    {
-        if(error)
-            *error = [NSError errorWithDomain:@"com.yourdomainhere" code:Z_DATA_ERROR userInfo:nil];
+    NSString *yourErrorDomain = @"com.yourdomainhere";
+    if (self.length < 18) {
+        if (error)
+            *error = [NSError errorWithDomain:yourErrorDomain code:Z_DATA_ERROR userInfo:nil];
         return nil;
     }
     z_stream zStream;
     memset(&zStream, 0, sizeof(zStream));
-    /*
-     * 16 is a magic number that allows inflate to handle gzip
-     * headers.
-     */
+
     int iResult = inflateInit2(&zStream, 16);
-    if(iResult != Z_OK)
-    {
-        if(error)
-            *error = [NSError errorWithDomain:@"com.yourdomainhere" code:iResult userInfo:nil];
+    if (iResult != Z_OK) {
+        if (error)
+            *error = [NSError errorWithDomain:yourErrorDomain code:iResult userInfo:nil];
         return nil;
     }
-    /*
-     * The last four bytes of a gzipped file/buffer contain the the number
-     * of uncompressed bytes expressed as a 32-bit little endian unsigned integer.
-     * See: RFC 1952 http://www.gzip.org/zlib/rfc-gzip.html
-     */
-    UInt32 nUncompressedBytes = *(UInt32*)(self.bytes + self.length - 4);
-    NSMutableData* gunzippedData = [NSMutableData dataWithLength:nUncompressedBytes];
+
+    UInt32 nUncompressedBytes = *(UInt32 *)(self.bytes + self.length - 4);
+    NSMutableData *gunzippedData = [NSMutableData dataWithLength:nUncompressedBytes];
     
-    zStream.next_in = (Bytef*)self.bytes;
+    zStream.next_in = (Bytef *)self.bytes;
     zStream.avail_in = (uint)self.length;
-    zStream.next_out = (Bytef*)gunzippedData.bytes;
+    zStream.next_out = (Bytef *)gunzippedData.bytes;
     zStream.avail_out = (uint)gunzippedData.length;
     
     iResult = inflate(&zStream, Z_FINISH);
-    if(iResult != Z_STREAM_END)
-    {
-        if(error)
-            *error = [NSError errorWithDomain:@"com.yourdomainhere" code:iResult userInfo:nil];
+    if (iResult != Z_STREAM_END) {
+        if (error)
+            *error = [NSError errorWithDomain:yourErrorDomain code:iResult userInfo:nil];
         gunzippedData = nil;
     }
     inflateEnd(&zStream);
